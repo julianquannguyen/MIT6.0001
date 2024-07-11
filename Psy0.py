23880f46cdf5b419c970cd11cd6f8a13a6574f1b
julianquannguyen <juliannguyenquan1412@gmail.com>
2024-07-11 08:03:04 -0400

    Add files via upload

---
+++ Psy0.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 11 07:36:13 2024
+
+@author: Julian Nguyen
+"""
+import numpy
+x = int(input('Enter number x: '))
+y = int(input('Enter number y: '))
+print('x**y = ', x**y)
+print('log(x) = ', int(numpy.log2(x))) 
